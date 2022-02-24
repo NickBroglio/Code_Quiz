5ceeac0e137a72efb118430bee4d64595ce142c4
Nick  Broglio <nickbroglio@Nicks-MacBook-Pro.local>
2022-02-24 15:29:14 -0500

    added end screen start screen and questions array

--- script.js
+++ script.js
@@ -1,77 +1,101 @@
 let viewHighScore = document.getElementById("view-highscore");
 let timerCounter = document.getElementById("timer-counter");
-let questionBox = document.getElementById("question-box");
+let questionContainer = document.getElementById("question-container");
 let startButton = document.getElementById("start-button");
+let correctAlert = document.getElementById("correct-alert");
 let timer;
 let timerCount = 100;
 let selections = [];
 let indexNumber = 0;
 let randomizedQuestions = [];
+let userChoice;
+let optionsEL = document.getElementById("options");
 
 
 // this array list all the questions and their answers
 let quizQuestions = [
     {
         question: "Commonly used data types DO NOT include:",
-        a: "strings",
-        b: "booleans",
-        c: "alerts",
-        d: "numbers",
-        correctAnswer: "c"
+        options: ["strings", "booleans", "alerts", "numbers"],
+        correctAnswer: "alerts"
     },
     {
         question: "The condition in an if / else statement is enclosed within ",
-        a: "quotes",
-        b: "curly brackets",
-        c: "parenthesis",
-        d: "square brackets",
-        correctAnswer: "c"
+        options: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        correctAnswer: "parenthesis"
     },
     {
         question: "Arrays in JavaScript can be used to store",
-        a: "numbers and strings",
-        b: "other arrays",
-        c: "booleans",
-        d: "all of the above",
-        correctAnswer: "d"
+        options: ["numbers and strings", "other arrays", "booleans", "all of the above",],
+        correctAnswer: "all of the above"
     },
     {
         question: "String values must be enclosed within _ when being assigned to variables",
-        a: "commas",
-        b: "curly brackets",
-        c: "quotes",
-        d: "paraenthesis",
-        correctAnswer: "c"
+        options: ["commas", "curly brackets", "quotes", "paraenthesis",],
+        correctAnswer: "quotes"
     },
 ]
-
+// once start button cliked, then timer starts
 startButton.addEventListener("click", startTimer);
 function startTimer() {
+    let startScreen = document.getElementById("start-screen");
+    console.log(startScreen)
+    // once function is run, the start screen hides 
+    startScreen.setAttribute("class", "hide");
+    questionContainer.removeAttribute("class")
+    // the timer is set to 100 seconds
     timer = setInterval(function () {
         timerCount--;
         timerCounter.textContent = "Time: " + timerCount;
         if (timerCount === 0) {
             clearInterval(timer);
         }
-        else if (timerCount > 0) {
-            passQuiz();
-        }
     }, 1000);
+    displayQuestions();
 }
 
-function orderQuestions() {
-    while (randomizedQuestions.length <= 4) {
-        let order = questions[Math.floor(Math.random() * questions.length)]
-        if (!randomizedQuestions.includes(order)) {
-            randomizedQuestions.push(order);
+function displayQuestions() {
+    let currentQuestion = quizQuestions[indexNumber];
+    let questionEL = document.getElementById("question");
+    questionEL.textContent = currentQuestion.question;
+    optionsEL.innerHTML = "";
+    currentQuestion.options.forEach(function (option, i) {
+        let optionBtn = document.createElement("button");
+        optionBtn.setAttribute("class", "option");
+        optionBtn.setAttribute("value", option);
+        optionBtn.textContent = i + 1 + ". " + option;
+        optionBtn.onclick = checkAnswer;
+        optionsEL.appendChild(optionBtn);
+    });
+}
+
+function checkAnswer() {
+    if (this.value !== quizQuestions[indexNumber].correctAnswer) {
+        timerCount -= 10;
+        if (timerCount < 0) {
+            timerCount = 0
         }
+    }
+    indexNumber++;
+    if (indexNumber === quizQuestions.length) {
+        endGame();
+        timerCount = 0
+    } else {
+        displayQuestions();
     }
 }
 
-function nextQuestion(){
-    
+
+
+
+function endGame() {
+    // hide questions show end screen
+    let endScreen = document.getElementById("end-screen");
+    endScreen.removeAttribute("class");
+    console.log("endgame");
 }
 
-
+// event listener for intial form
+// get inital value and put in local storage
 
 
